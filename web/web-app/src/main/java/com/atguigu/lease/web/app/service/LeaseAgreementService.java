742454e9fdcf2a327aa14c231fdbf153c0a2c1fd
Yuxuan <1612154183@qq.com>
2024-08-06 09:55:23 +0800

    feat(app-lease):realize all the function in the leaseAgreement module
    实现了租约部分的所有接口，包括分页获取个人租约基本信息列表，根据id获取租约详细信息，根据id更新租约状态，以及保存或更新租约信息

--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/LeaseAgreementService.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/LeaseAgreementService.java
@@ -1,7 +1,12 @@
 package com.atguigu.lease.web.app.service;
 
 import com.atguigu.lease.model.entity.LeaseAgreement;
+import com.atguigu.lease.model.enums.LeaseStatus;
+import com.atguigu.lease.web.app.vo.agreement.AgreementDetailVo;
+import com.atguigu.lease.web.app.vo.agreement.AgreementItemVo;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -9,4 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2023-07-26 11:12:39
 */
 public interface LeaseAgreementService extends IService<LeaseAgreement> {
+    List<AgreementItemVo> listItem(String phone);
+
+    AgreementDetailVo getDetailById(Long id);
+
+    void updateStatus(Long id, LeaseStatus leaseStatus);
 }
--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LeaseAgreementServiceImpl.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LeaseAgreementServiceImpl.java
@@ -1,10 +1,17 @@
 package com.atguigu.lease.web.app.service.impl;
 
 import com.atguigu.lease.model.entity.LeaseAgreement;
+import com.atguigu.lease.model.enums.LeaseStatus;
 import com.atguigu.lease.web.app.mapper.LeaseAgreementMapper;
 import com.atguigu.lease.web.app.service.LeaseAgreementService;
+import com.atguigu.lease.web.app.vo.agreement.AgreementDetailVo;
+import com.atguigu.lease.web.app.vo.agreement.AgreementItemVo;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author liubo
@@ -15,6 +22,25 @@ import org.springframework.stereotype.Service;
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
 
+    @Resource
+    private LeaseAgreementMapper agreementMapper;
+
+    @Override
+    public List<AgreementItemVo> listItem(String phone) {
+        return agreementMapper.listItem(phone);
+    }
+
+    @Override
+    public AgreementDetailVo getDetailById(Long id) {
+        return agreementMapper.getDetailById(id);
+    }
+
+    @Override
+    public void updateStatus(Long id, LeaseStatus leaseStatus) {
+        LambdaUpdateWrapper<LeaseAgreement> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(LeaseAgreement::getId,id).set(LeaseAgreement::getStatus,leaseStatus);
+        this.update(updateWrapper);
+    }
 }
 
 
